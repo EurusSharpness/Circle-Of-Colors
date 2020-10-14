7ac84647b24499eeb6f067047a9da8a738a05424
Same Eldin <samheldin.1999@gmail.com>
2020-10-14 17:24:54 +0300

    Add project files.

---
+++ Circle Of Colors/MainClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Of_Colors
+{
+    class MainClass
+    {
+        public MainClass()
+        {
+
+        }
+
+        public void Draw(Graphics g)
+        {
+
+        }
+    }
+}
